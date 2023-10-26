65263a964382880c5a1d832c331fa1ff63e4bb52
Oskar Brzeski <oskar.brzeski.github@hotmail.com>
2023-10-26 21:43:59 +0100

    set up linting with black

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import src.api as api
-
 from datetime import datetime, timezone
 
+import src.api as api
+
 if __name__ == "__main__":
-    maps = api.get_all_leaderboard_maps(upto = datetime(2009, 1, 1, tzinfo=timezone.utc))
-    
+    maps = api.get_all_leaderboard_maps(upto=datetime(2009, 1, 1, tzinfo=timezone.utc))
+
     for map in maps:
-        print(f"{map.approved_date} | id={map.beatmap_id} | set={map.beatmapset_id} | {map.title}")
+        print(
+            f"{map.approved_date} | id={map.beatmap_id} | set={map.beatmapset_id} | {map.title}"
+        )
