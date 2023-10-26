5fe744348b13c1f2a20fcb017950bf3a5263f217
Oskar Brzeski <oskar.brzeski.github@hotmail.com>
2023-10-26 20:59:32 +0100

    get leaderboard maps between datetimes

---
+++ main.py
@@ -0,0 +1,9 @@
+import src.api as api
+
+from datetime import datetime, timezone
+
+if __name__ == "__main__":
+    maps = api.get_all_leaderboard_maps(upto = datetime(2009, 1, 1, tzinfo=timezone.utc))
+    
+    for map in maps:
+        print(f"{map.approved_date} | id={map.beatmap_id} | set={map.beatmapset_id} | {map.title}")
---
+++ src/api.py
@@ -0,0 +1,38 @@
+from datetime import datetime, timedelta, timezone
+from typing import Final
+import os
+
+from dotenv import load_dotenv
+import ossapi
+from ossapi.ossapi import Beatmap as BeatmapV1
+
+
+load_dotenv(".env")
+API_URL: Final[str] = "https://osu.ppy.sh/api/"
+API_LEGACY_KEY: Final[str] = os.environ.get("LEGACY_OSU_API_KEY")
+API_CLIENT_ID: Final[int] = int(os.environ.get("CLIENT_ID"))
+API_CLIENT_SECRET: Final[str] = os.environ.get("CLIENT_SECRET")
+
+
+API_V1 = ossapi.OssapiV1(API_LEGACY_KEY)
+
+
+def get_all_leaderboard_maps(
+        since: datetime = datetime(2007, 1, 1, tzinfo=timezone.utc),
+        upto: datetime = datetime.now(timezone.utc) + timedelta(days=1),
+    ) -> list[BeatmapV1]:
+
+    maps: list[BeatmapV1] = []
+    map_id_set: set[int] = set()
+
+    while (retrieved := API_V1.get_beatmaps(since=since)):
+        for map in retrieved:
+            if map.beatmap_id in map_id_set: continue
+            if map.approved_date > upto: return maps
+
+            maps.append(map)
+            map_id_set.add(map.beatmap_id)
+        
+        since = maps[-1].approved_date - timedelta(seconds=1)
+    
+    return maps
