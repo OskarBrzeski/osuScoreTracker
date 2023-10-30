8b7be96f3b6238d67b8a0a76edbfcef8a60245b3
Oskar Brzeski <oskar.brzeski.github@hotmail.com>
2023-10-30 18:42:39 +0000

    add header line to csv file

--- src/database.py
+++ src/database.py
@@ -199,12 +199,25 @@ def export_scores_as_csv(cursor: sql.Cursor, user_id: int) -> None:
 
     with open(f"export/scores-{user_id}.csv", "w", newline="") as file:
         writer = csv.writer(file)
+        writer.writerow(
+            (
+                "Score ID",
+                "User ID",
+                "Map ID",
+                "Score",
+                "Accuracy",
+                "Combo",
+                "Mods",
+                "Time Submitted",
+                "pp",
+            )
+        )
         writer.writerows(scores)
 
 
 def import_scores_from_csv(csv_path: str) -> None:
-    """Adds scores from csv file into database."""
+    """Adds scores from CSV file into database."""
     with open(csv_path) as file:
         reader = csv.reader(file)
-        for row in reader:
+        for row in reader[1:]:
             add_score(tuple(row))
